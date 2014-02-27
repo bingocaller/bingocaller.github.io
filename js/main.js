9169c4c75009b7739542294c652bffb2cac729ab
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2014-02-27 10:08:31 +0100

    Testing with proper contenta

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.menu li').click(function() {
+		var clicked = $(this).find('a').attr('href').replace('#','');
+		$('.menu li').removeClass('active');
+		$(this).addClass('active');
+		$('.copy').fadeOut(200, function () {
+			$('.copy > div').addClass('not-active').removeClass('active');
+			$('.copy .'+clicked).addClass('active').removeClass('not-active');
+			$('.copy').fadeIn(200, function () {
+				if ($(document).width() <= 770){
+					$(window).scrollTop($('.menu').height()+$('.header').height());
+				}
+			});
+		});
+	});
+});
