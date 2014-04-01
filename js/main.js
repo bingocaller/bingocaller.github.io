f20122fef9c9cded2dc6c6c59da799fcdbbb405f
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2014-04-01 12:56:10 +0200

    Changed functionality to rely on history.onpopstate

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-$(document).ready(function() {
-	$('.menu li').click(function() {
-		var clicked = $(this).find('a').attr('href').replace('#','');
-		$('.menu li').removeClass('active');
-		$(this).addClass('active');
-		$('.copy').fadeOut(200, function () {
-			$('.copy > div').addClass('not-active').removeClass('active');
-			$('.copy .'+clicked).addClass('active').removeClass('not-active');
-			$('.copy').fadeIn(200, function () {
-				if ($(document).width() <= 770){
-					$(window).scrollTop($('.menu').height()+$('.header').innerHeight());
-				}
-			});
+function getHash () {
+	if (window.location.hash) {
+		var hash = window.location.hash.replace('#', '');
+		// console.log(hash);
+		fader(hash);
+	} else {
+		var hash = 'experience';
+		fader(hash);
+	}
+}
+function fader (target) {
+	$('.menu li').removeClass('active');
+	$('.menu a[href=#' + target +']').parent('li').addClass('active');
+	$('.copy').fadeOut(200, function () {
+		$('.copy > div').addClass('not-active').removeClass('active');
+		$('.copy .' + target).addClass('active').removeClass('not-active');
+		$('.copy').fadeIn(200, function () {
+			if ($(document).width() <= 770){
+				$(window).scrollTop($('.menu').height()+$('.header').innerHeight());
+			}
 		});
 	});
+}
+$(document).ready(function() {
+	getHash();
+	window.onpopstate = function() {
+		getHash();
+	}
 });
