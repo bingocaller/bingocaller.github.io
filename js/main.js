8808c48fa7289e79c4a9c3f084c9d64298ba5552
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2016-04-07 22:12:37 +0200

    Update a whole bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,40 +1,39 @@
 function getHash () {
-	if (location.hash) {
-		var hash = location.hash.replace('#', '');
-		// console.log(hash);
-		fader(hash);
-	} else {
-		var hash = 'experience';
-		fader(hash);
-	}
+  var hash = 'experience';
+  if (location.hash) {
+    hash = location.hash.replace('#', '');
+    fader(hash);
+  } else {
+    fader(hash);
+  }
 }
 function fader (target) {
-	$('.menu li').removeClass('active');
-	$('.menu a[href=#' + target +']').parent('li').addClass('active');
-	$('.copy').fadeOut(200, function () {
-		$('.copy > div').addClass('not-active').removeClass('active');
-		$('.copy .' + target).addClass('active').removeClass('not-active');
-		$('.copy').fadeIn(200, function () {
-			if ($(document).width() <= 770 && location.hash){
-				$(window).scrollTop($('.menu').height()+$('.header').innerHeight());
-			}
-		});
-	});
+  $('.menu li').removeClass('active');
+  $('.menu a[href=#' + target +']').parent('li').addClass('active');
+  $('.copy').fadeOut(200, function () {
+    $('.copy > div').addClass('not-active').removeClass('active');
+    $('.copy .' + target).addClass('active').removeClass('not-active');
+    $('.copy').fadeIn(200, function () {
+      if ($(document).width() <= 770 && location.hash){
+        $(window).scrollTop($('.menu').height()+$('.header').innerHeight());
+      }
+    });
+  });
 }
 $(document).ready(function() {
-	getHash();
-	window.onhashchange = function() {
-		getHash();
-	}
-	$('.menu li').on('click', function() {
-		var href = $(this).find('a').attr('href');
-		if (window.location.origin.indexOf('undefined') !== -1) {
-			window.location.href = window.location.origin.replace('undefined','') + '/' + href;
-		} else {
-			window.location.href = window.location.origin + '/' + href;
-		}
-	});
-	$('.menu li a').on('click', function(event) {
-		event.stopPropagation();
-	});
+  getHash();
+  window.onhashchange = function() {
+    getHash();
+  }
+  $('.menu li').on('click', function() {
+    var href = $(this).find('a').attr('href');
+    if (window.location.origin.indexOf('undefined') !== -1) {
+      window.location.href = window.location.origin.replace('undefined','') + '/' + href;
+    } else {
+      window.location.href = window.location.origin + '/' + href;
+    }
+  });
+  $('.menu li a').on('click', function(event) {
+    event.stopPropagation();
+  });
 });
